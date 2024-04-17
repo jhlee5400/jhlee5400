65d4373979765fe3b6ccf07584b1df8c4705b365
jhlee5400 <162522321+jhlee5400@users.noreply.github.com>
2024-04-17 22:36:08 +0900

    Create HW5.py

---
+++ HW5.py
@@ -0,0 +1,15 @@
+# 1번 #
+# 출력: print(), write(), writeline()
+# 입력: input(), read(), readline(), readlines()
+
+# 2번 #
+# 정답: 1번 (A-C-B-D)
+
+# 3번 #
+# 정답: 3(a는 쓰기모드이다. 기존 파일 있으면 삭제하고 새로 만든다.)
+
+# 4번 #
+# 정답: 1 - open, 2 - readline, 3 - close
+
+# 6번 #
+# 정답: 1 - inFp.readlines(), 2 - outFp.write(inStr)
