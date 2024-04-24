4665d030a47bb68159308bfdaf7d77c3531e7159
jhlee5400 <162522321+jhlee5400@users.noreply.github.com>
2024-04-24 22:45:02 +0900

    Create HW7.py

---
+++ HW7.py
@@ -0,0 +1,18 @@
+# 2번
+class Car:
+  color = ""
+  speed = 0
+  def upSpeed(self,value):
+    self.speed += value
+
+# 4번
+class Horse:
+  speed = 0
+  def__init__(self):
+    self.speed = 50
+
+# 6번
+# 정답: 3번(슈퍼 클래스 서브 클래스)
+
+# 7번 
+# 정답: class RVCar(Car)
